de42aa01eb69d4d7d043dbcacd63bf21dbabe197
hitesh bhargav <hiteshbhargav420@gmail.com>
2020-05-01 18:01:55 +0530

    Updates

---
+++ precache-manifest.f9b84c31c76a628a428bedf7ba4ca8e2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4fd98f3f7c357b612f61548de5460f2",
+    "url": "/theGuardians/index.html"
+  },
+  {
+    "revision": "bedad9aedca3f50f2726",
+    "url": "/theGuardians/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "5217d3e102e7ee313436",
+    "url": "/theGuardians/static/css/main.bdebf64c.chunk.css"
+  },
+  {
+    "revision": "bedad9aedca3f50f2726",
+    "url": "/theGuardians/static/js/2.ee7c2b25.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/theGuardians/static/js/2.ee7c2b25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5217d3e102e7ee313436",
+    "url": "/theGuardians/static/js/main.5742779c.chunk.js"
+  },
+  {
+    "revision": "ce799c4b928bfe230bc4",
+    "url": "/theGuardians/static/js/runtime-main.cf423bf0.js"
+  },
+  {
+    "revision": "4d503d3690424d417e0a6b1035eb1af6",
+    "url": "/theGuardians/static/media/BrexiStoneRegular-JRRmB.4d503d36.woff"
+  }
+]);
